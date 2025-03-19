1ca637526a4a7f7db98312c1f58c5977767b1861
unknown <heliomatto@gmail.com>
2025-03-19 07:16:16 -0300

    aula 05 incompleta

--- ProjetoP2/todos/views.py
+++ ProjetoP2/todos/views.py
@@ -3,6 +3,16 @@ from .models import Todo
 
 
 # Create your views here.
+from django.views.generic import ListView,  CreateView
+
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+ model = Todo
+ fields = ["title", "deadline"]
+
+
 def todo_list(requast):
     todos = Todo.objects.all()
 
