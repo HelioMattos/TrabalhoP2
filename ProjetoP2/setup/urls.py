702b2764a01778174bb716a3df83a8bcb59fd5eb
HelioMattos <heliomatto@gmail.com>
2025-03-21 10:13:55 -0300

    aula 5 fnalizada

--- ProjetoP2/setup/urls.py
+++ ProjetoP2/setup/urls.py
@@ -18,12 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from todos.views import todo_list
 
-from todos.views import TodoListView
-from todos.views import TodoCreateView
+from todos.views import TodoListView, TodoCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ("", todo_list),
-    path("", TodoListView.as_view()),
-    path("create", TodoCreateView.as_view())
+    path('', TodoListView.as_view(), name="todo_list"),
+     path('create', TodoCreateView.as_view(), name="todo_create")
 ]
--- ProjetoP2/todos/views.py
+++ ProjetoP2/todos/views.py
@@ -4,13 +4,15 @@ from .models import Todo
 
 # Create your views here.
 from django.views.generic import ListView,  CreateView
+from django.urls import reverse_lazy
 
 class TodoListView(ListView):
-    model = Todo
+    model = Todo;
 
 class TodoCreateView(CreateView):
- model = Todo
+ model = Todo;
  fields = ["title", "deadline"]
+ success_url = reverse_lazy("todo_list");
 
 
 def todo_list(requast):
